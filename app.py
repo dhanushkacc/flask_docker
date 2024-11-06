ca2cedd8cb847760df6a7e1799bd54adf421940d
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2024-11-06 21:58:34 +0530

    update root

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
     if not operation or not num1 or not num2:
         return '''
        
-        Use the following format to calculate:
+         the following format to calculate:
         /?operation=<operation>&num1=<number1>&num2=<number2>
         
         Supported operations: add, subtract, multiply, divide
